21f9c5abdefe74d5feeb6ae1c150609d7c944a54
patrickjacob <patrickjacob@10.132.79.233>
2016-09-23 07:38:28 +1000

    game

--- src/rmit_pf_ass2/Game.java
+++ src/rmit_pf_ass2/Game.java
@@ -1,5 +1,16 @@
 package rmit_pf_ass2;
 
 public class Game {
+	public static void main(String args[]){
 
+	private int menu () 
+	private int menu_choice;
+	System.out.println("=====Wumpus====");
+	System.out.println("1. Move player left");
+	System.out.println("2. Move player right");
+	System.out.println("3. Move player up");
+	System.out.println("4. Move player down");
+	System.out.println("5. Quit");
+	return menu_choice;
+}
 }
