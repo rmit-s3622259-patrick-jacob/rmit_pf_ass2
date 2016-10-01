bf7984d0545f134626e38ca1a93c03a835d24528
s3021786 <s3021786@student.rmit.edu.au>
2016-10-01 17:52:47 +1000

    tracking score and some println's

--- src/rmit_pf_ass2/Gold.java
+++ src/rmit_pf_ass2/Gold.java
@@ -1,4 +1,5 @@
 package rmit_pf_ass2;
+
 import java.util.*;
 /* From the assignment description
  * Gold is displayed as 'g',
@@ -12,34 +13,46 @@ import java.util.*;
  */
 
 
-public class Gold extends GameItem{
-/*	
-	int randRow;
-	int randColumn;
-	do {
-	  randRow = random.nextInt(ROWS);
-	  randColumn = random.nextInt(COLUMNS);
-	} while (grid[randRow][randColumn] != null);
-	grid[randRow][randColumn] = "X";
-*/
+public class Gold extends GameItem {
+	
+	public int goldScore = 0;
+	
+	/*
+	 * int randRow; int randColumn; do { randRow = random.nextInt(ROWS);
+	 * randColumn = random.nextInt(COLUMNS); } while (grid[randRow][randColumn]
+	 * != null); grid[randRow][randColumn] = "X";
+	 */
 	public int[][] getGoldposition() {
-	int ag = 0; //position gold as array horizontal
-		
+		int ag = 0; // position gold as array horizontal
+
 		Random amountGoldGenerator = new Random();
-		ag = amountGoldGenerator.nextInt(2); 
-		
-	int[][] pg = new int[ag][2];
-	int i;
-	//Random Generator to get a random number between 0 and 3 using the Random Method
-	//do {
-		for (i=0; i < ag; i++ ){
-			for (int j=0; j<2; j++) {
-			Random GoldGenerator = new Random();
-			pg[i][j] = GoldGenerator.nextInt(3); 
+		ag = amountGoldGenerator.nextInt(2);
+
+		int[][] pg = new int[ag][2];
+		int i;
+		// Random Generator to get a random number between 0 and 3 using the
+		// Random Method
+		// do {
+		for (i = 0; i < ag; i++) {
+			for (int j = 0; j < 2; j++) {
+				Random GoldGenerator = new Random();
+				pg[i][j] = GoldGenerator.nextInt(3);
 			}
 		}
-	//}
-	//while ();
-			return pg;
-		}
+		// }
+		// while ();
+		return pg;
+	}
+
+	// this method when invoked will notify the player has landed on gold
+
+	public void sendOutcome() {
+		System.out.println("Player landed in gold");
+		increaseScore();
+	}
+	
+	public void increaseScore(){
+		goldScore= goldScore++;
+		System.out.println("Player landed in gold. Score is" +goldScore);
+	}
 }
--- src/rmit_pf_ass2/Pit.java
+++ src/rmit_pf_ass2/Pit.java
@@ -2,4 +2,11 @@ package rmit_pf_ass2;
 
 public class Pit extends GameItem {
 
+	//this method when invoked will notify the player has landed in the pit
+	
+	public void sendOutcome(){
+		System.out.println("Player landed in pit");
+		
+		
+	}
 }
